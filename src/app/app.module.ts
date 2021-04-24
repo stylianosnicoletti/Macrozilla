a42286f641a9ae8d8fdc542fe312a944c6463b9b
Stelios <lakis94@gmail.com>
2021-04-24 13:49:13 +0300

    Angular pwa configuration. Trying to remove cached on deploy. Icons updated.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,7 +30,13 @@ import { ServiceWorkerModule } from '@angular/service-worker';
             BrowserModule,
             IonicModule.forRoot(),
             AppRoutingModule,
-            ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
+            ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+            ServiceWorkerModule.register('ngsw-worker.js', {
+              enabled: environment.production,
+              // Register the ServiceWorker as soon as the app is stable
+              // or after 30 seconds (whichever comes first).
+              registrationStrategy: 'registerWhenStable:30000'
+            })],
   providers: [
     StatusBar,
     GooglePlus,

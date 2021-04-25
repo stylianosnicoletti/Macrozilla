b7dc45b24a181f2bba96cfed6ebeddf8c97efcf4
Stelios <lakis94@gmail.com>
2021-04-25 18:40:16 +0300

    Fixing compilation Jasmine Karma tests..

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,21 +1,35 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { TestBed, async } from '@angular/core/testing';
-
+import { CUSTOM_ELEMENTS_SCHEMA, NgZone } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
 import { AppComponent } from './app.component';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Observable, Subject } from 'rxjs';
+import { Mock } from 'protractor/built/driverProviders';
 
 describe('AppComponent', () => {
 
-  let statusBarSpy, splashScreenSpy, platformReadySpy, platformSpy;
+  let statusBarSpy;
+  let splashScreenSpy;
+  let platformReadySpy;
+  let platformPauseResumeSubject: Subject<void>;
+  let platformSpy;
+  let angularFireDatabaseGoOfflineSpy;
+  let angularFireDatabaseGoOnlineSpy;
+  let angularFireDatabaseSpy;
 
-  beforeEach(async(() => {
+  beforeEach((() => {
     statusBarSpy = jasmine.createSpyObj('StatusBar', ['styleDefault']);
     splashScreenSpy = jasmine.createSpyObj('SplashScreen', ['hide']);
     platformReadySpy = Promise.resolve();
-    platformSpy = jasmine.createSpyObj('Platform', { ready: platformReadySpy });
+    platformPauseResumeSubject =  new Subject();
+    platformSpy = jasmine.createSpyObj('Platform',
+                  {ready: platformReadySpy},
+                  {pause: platformPauseResumeSubject.asObservable(), resume: platformPauseResumeSubject.asObservable() });
+    angularFireDatabaseGoOfflineSpy = jasmine.any;
+    angularFireDatabaseGoOnlineSpy = jasmine.any;
+    angularFireDatabaseSpy = jasmine.createSpyObj('AngularFireDatabase.database', { goOffline: angularFireDatabaseGoOfflineSpy, goOnline: angularFireDatabaseGoOnlineSpy});
 
     TestBed.configureTestingModule({
       declarations: [AppComponent],
@@ -24,17 +38,18 @@ describe('AppComponent', () => {
         { provide: StatusBar, useValue: statusBarSpy },
         { provide: SplashScreen, useValue: splashScreenSpy },
         { provide: Platform, useValue: platformSpy },
+        { provide: AngularFireDatabase, useValue: angularFireDatabaseSpy }
       ],
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  it('Should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it('should initialize the app', async () => {
+  it('Should initialize the app', async () => {
     TestBed.createComponent(AppComponent);
     expect(platformSpy.ready).toHaveBeenCalled();
     await platformReadySpy;

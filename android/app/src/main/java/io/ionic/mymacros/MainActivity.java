ee992bb9edfc0c0cfa94144196e062e53b9fa41c
Stelios <lakis94@gmail.com>
2021-04-27 22:32:43 +0300

    Force dark theme when enabled in Android. CHnaged name. Force portrait.

--- android/app/src/main/java/io/ionic/mymacros/MainActivity.java
+++ android/app/src/main/java/io/ionic/mymacros/MainActivity.java
@@ -1,10 +1,12 @@
 package io.ionic.mymacros;
 
+import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
-
+import android.webkit.WebSettings;
+import androidx.annotation.RequiresApi;
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
-
 import java.util.ArrayList;
 
 public class MainActivity extends BridgeActivity {
@@ -17,5 +19,23 @@ public class MainActivity extends BridgeActivity {
       // Additional plugins you've installed go here
       // Ex: add(TotallyAwesomePlugin.class);
     }});
+  }
+
+  /// Force Dark Mode on App when it is enabled on Android
+  // Android versions >=10 required
+  @RequiresApi(api = Build.VERSION_CODES.Q)
+  @Override
+  public void onResume() {
+    super.onResume();
+    int nightModeFlags = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+    WebSettings webSettings = this.bridge.getWebView().getSettings();
+
+    if (nightModeFlags == Configuration.UI_MODE_NIGHT_YES) {
+      if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+        webSettings.setForceDark(WebSettings.FORCE_DARK_ON);
+      }
+    } else {
+      webSettings.setForceDark(WebSettings.FORCE_DARK_OFF);
+    }
   }
 }

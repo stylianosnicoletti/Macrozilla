d6472b9dc07de20d7a6e0a0604c36ccf45509a71
Stelios <lakis94@gmail.com>
2021-04-28 23:31:02 +0300

    Assets for android created. Status bar fix.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,7 +26,7 @@ export class AppComponent {
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
+      this.statusBar.show();
       this.splashScreen.hide();
     });
   }

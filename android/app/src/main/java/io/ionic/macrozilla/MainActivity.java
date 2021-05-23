b02c064b9e86f166e2a0bbcb0800b189e88d79f2
Stelios <lakis94@gmail.com>
2021-05-23 15:21:13 +0300

    fixed capacitor splash screen, showing when keyboard pops up

--- android/app/src/main/java/io/ionic/macrozilla/MainActivity.java
+++ android/app/src/main/java/io/ionic/macrozilla/MainActivity.java
@@ -1,5 +1,14 @@
 package io.ionic.macrozilla;
 
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Initializes the Bridge
+        }
+    }

d0a9e4270c86e0e97f2e45164c4bb761084f060e
Stelios <lakis94@gmail.com>
2021-05-27 23:11:59 +0300

    maintenance updates, app availability

--- src/app/models/maintenance.model.ts
+++ src/app/models/maintenance.model.ts
@@ -4,12 +4,17 @@
 export interface Maintenance {
 
     Enabled: boolean;
-    
+
     EnabledMessage: string;
-    
-    UpdateLatestMajor: string;
-    
-    UpdateMessage: string;
-    
-    UpdateUrl: string; 
+    /**
+     * Latest Android version (E.g. 2.13) -> (2: Major, 1: Minor, 3: Patch).
+     * Latest Web version (E.g. 2.1.3) -> (2: Major, 1: Minor, 3: Patch)
+     */
+    UpdateLatestVersion: string;
+
+    UpdateMessageMajor: string;
+
+    UpdateMessageMinor: string;
+
+    UpdateUrl?: string;
 }

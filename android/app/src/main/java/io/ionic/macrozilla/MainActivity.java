6537b905cfa9b660c5f0792ee06ef8fb9a6d3d27
stylianosnicoletti <lakis94@gmail.com>
2022-08-04 18:43:16 +0300

    update android folder

--- android/app/src/main/java/io/ionic/macrozilla/MainActivity.java
+++ android/app/src/main/java/io/ionic/macrozilla/MainActivity.java
@@ -1,12 +1,5 @@
 package io.ionic.macrozilla;
 
-import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-        @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // Initializes the Bridge
-        }
-    }
+public class MainActivity extends BridgeActivity {}

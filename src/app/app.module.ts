16ad09ec21a8f582be86e249a3d903c30fbdbc63
stylianosnicoletti <35661905+stylianosnicoletti@users.noreply.github.com>
2022-08-09 22:59:18 +0300

    Update app.module.ts
    Only disable back button in web branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,10 +22,11 @@ import { BackButtonDisableModule } from './other-modules/disable-browser-back-bu
             BrowserModule,
             IonicModule.forRoot(),
             AppRoutingModule,
-            // To disable the browser back button
-            BackButtonDisableModule.forRoot({
-              preserveScrollPosition: true
-            }),
+            // ONLY WEB BRANCH !!!
+            // To disable the back button
+            // BackButtonDisableModule.forRoot({
+            //  preserveScrollPosition: true
+            // }),
             ServiceWorkerModule.register('ngsw-worker.js', {
               enabled: environment.production,
               registrationStrategy: 'registerImmediately'

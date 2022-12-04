e901ae07998cf0b3d5fe97100bfb62f34b617da3
Stelios Nicoletti <stelios@Stelios-Desktop.localdomain>
2022-12-04 20:54:14 +0200

    all into main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireAuthModule} from '@angular/fire/compat/auth';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { environment } from '../environments/environment';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
@@ -15,28 +15,30 @@ import { BackButtonDisableModule } from './other-modules/disable-browser-back-bu
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [AngularFireModule.initializeApp(environment.firebase),
-            AngularFireAuthModule,
-            AngularFirestoreModule,
-            HttpClientModule,
-            BrowserModule,
-            IonicModule.forRoot(),
-            AppRoutingModule,
-            // To disable the browser back button
-            BackButtonDisableModule.forRoot({
-              preserveScrollPosition: true
-            }),
-            ServiceWorkerModule.register('ngsw-worker.js', {
-              enabled: environment.production,
-              registrationStrategy: 'registerImmediately'
-            }),
-          ],
+  imports: [
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    HttpClientModule,
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    // To disable the browser back button
+    (environment.platform == "web") ?
+      BackButtonDisableModule.forRoot({
+        preserveScrollPosition: true
+      }) : [],
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      registrationStrategy: 'registerImmediately'
+    }),
+  ],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  
+
 }
 

79b97fb7f8b454da1afb02324aafcd4d7eb36ed7
Stelios Nicoletti <stelios@Stelios-Desktop.localdomain>
2022-12-04 17:40:48 +0200

    splash hide on log in guard

--- src/app/guards/can-enter-login-page.guard.ts
+++ src/app/guards/can-enter-login-page.guard.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, RouterStateSnapshot, Router, CanActivate } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { map } from "rxjs/operators";
+import { Capacitor } from '@capacitor/core';
+import { SplashScreen } from '@capacitor/splash-screen';
+import { StatusBar } from '@capacitor/status-bar';
 
 
 @Injectable({
@@ -14,6 +17,16 @@ export class CanEnterLoginPageGuard implements CanActivate {
     stateSnapshot: RouterStateSnapshot) {
     return this._angularFireAuth.authState.pipe(
       map((auth) => {
+        const platform = Capacitor.getPlatform();    
+        // Native Platform (Android/iOS)
+        if (Capacitor.isNativePlatform()) {
+          //console.log("Is Native");
+          //console.log(`Hide splash screen`);
+          SplashScreen.hide().then(() => { 
+            //console.log(`Splashscreen hidden`);
+           });
+          StatusBar.show();
+        }
         //console.log("CanEnterLoginPageGuard");
         if (auth && auth.emailVerified) {
           this._router.navigate(["/authorized_user/tabs/daily_entry"]);

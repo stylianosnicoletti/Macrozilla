66a61d2b9f50e8cef16022350a7735974cb0fa50
Stelios Nicoletti <stelios@Stelios-Desktop.localdomain>
2022-12-04 18:11:44 +0200

    try again button no network

--- src/app/components/authorized-user/authorized-user.page.ts
+++ src/app/components/authorized-user/authorized-user.page.ts
@@ -25,21 +25,12 @@ export class AuthorizedUserPage {
    * Will not be triggered, if you come back to a page after putting it into a stack.
    */
   async ngOnInit() {
-  //console.log("ngOnInit AuthorizedUserPage.");
+    //console.log("ngOnInit AuthorizedUserPage.");
     await (await this._userService.getUserFields()).subscribe(async x => {
       this._renderer.setAttribute(document.body, 'color-theme', this.mapThemeModeToBodyName(x.Options.DarkMode))
     });
 
-    this.networkListener = Network.addListener('networkStatusChange', async status => {
-      if (status.connected && !this.lastNetworkStatusIsConnected) {
-        //console.log('Network connected!');
-        this.lastNetworkStatusIsConnected = true;
-      }
-      else if (!status.connected) {
-        //console.log('Network disconnected!');
-        this.lastNetworkStatusIsConnected = false;
-      }
-    });
+    this.networkListener = await this.initNetworkListener();
   }
 
   async ionViewWillEnter() {
@@ -69,6 +60,28 @@ export class AuthorizedUserPage {
       return 'dark';
     }
     return 'light';
+  }
+
+  async resetNetworkListener() {
+    if (this.networkListener) {
+      this.networkListener.remove();
+    }
+    this.lastNetworkStatusIsConnected = await (await Network.getStatus()).connected;
+    //console.log(this.lastNetworkStatusIsConnected);
+    this.networkListener = await this.initNetworkListener();
+  }
+
+  async initNetworkListener(): Promise<PluginListenerHandle> {
+    return await Network.addListener('networkStatusChange', async status => {
+      if (status.connected && !this.lastNetworkStatusIsConnected) {
+        //console.log('Network connected!');
+        this.lastNetworkStatusIsConnected = true;
+      }
+      else if (!status.connected) {
+        //console.log('Network disconnected!');
+        this.lastNetworkStatusIsConnected = false;
+      }
+    });
   }
 
 }
